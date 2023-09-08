119f9c06c7a5fde4e67750c0cfa75d28b024f28e
1033394 <144168815+1033394@users.noreply.github.com>
2023-09-08 12:10:44 +0530

    Add files via upload

---
+++ day3 assignment2/q1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3_assignment2
+{
+    class q1
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            double average = CalculateAverage(arr);
+            int minimum = FindMinimum(arr);
+            int maximum = FindMaximum(arr);
+            Console.WriteLine("Array: [" + string.Join(",", arr) + "]");
+            Console.ReadLine();
+            Console.WriteLine("Average value: " + average);
+            Console.ReadLine();
+            Console.WriteLine("Minimum value: " + minimum);
+            Console.ReadLine();
+            Console.WriteLine("Maximum value: " + maximum);
+            Console.ReadLine();
+
+
+        }
+        static double CalculateAverage(int[] arr)
+        {
+            double sum = 0;
+            foreach (int num in arr)
+            {
+                sum += num;
+            }
+            return sum / arr.Length;
+        }
+        static int FindMinimum(int[] arr)
+        {
+            int min = arr[0];
+            foreach(int num in arr)
+            {
+                if(num<min)
+                {
+                    min = num;
+                }
+            }
+            return min;
+        }
+        static int FindMaximum(int[] arr)
+        {
+            int max = arr[0];
+            foreach (int num in arr)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            return max;
+        }
+    }
+}
