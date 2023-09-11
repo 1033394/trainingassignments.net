a6b532b2f59ded7a5d02ff5f79901c42d4201bf9
1033394 <144168815+1033394@users.noreply.github.com>
2023-09-11 10:53:33 +0530

    Add files via upload

---
+++ codebased/q2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q2
+{
+    class q2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a no: ");
+            int no = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Multiplication Table for { no}:");
+            for (int i=1;i<=10;i++)
+            {
+                int result = no * i;
+                Console.WriteLine($"{no} x {i}={result}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
