333242c8f60134ffa53186b0990130395d4c7d10
1033394 <144168815+1033394@users.noreply.github.com>
2023-10-06 14:45:24 +0530

    Add files via upload

---
+++ csharp assignments/assignment2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = new int[10];
+            int total = 0;
+            Console.WriteLine("Enter numbers:");
+            for (int i=0;i<10;i++)
+            {
+                Console.WriteLine($"Enter marks {i+1}:");
+                marks[i] = Convert.ToInt32(Console.ReadLine());
+                total += marks[i];
+
+            }
+            double average = (double)total / 10;
+            int minMark = marks[0];
+            int maxMark = marks[0];
+            foreach(int mark in marks)
+            {
+                if (mark < minMark)
+                    minMark = mark;
+                if (mark < maxMark)
+                    maxMark = mark;
+
+            }
+            Array.Sort(marks);
+            Array.Reverse(marks);
+            Console.WriteLine($"Total: {total}");
+            Console.WriteLine($"Average {i + 1}:");
+
+        }
+    }
+}
---
+++ csharp assignments/assignment2/q2/q2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q2
+{
+    class q2
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = new int[10];
+            for (int i=0;i<10; i++)
+            {
+                Console.WriteLine($"Enter mark {i + 1}: ");
+                marks[i] = int.Parse(Console.ReadLine());
+            }
+            int total = 0;
+            foreach (int mark in marks)
+            {
+                total += mark;
+            }
+            Console.WriteLine($"a.Total: {total}");
+            double average = (double)total / marks.Length;
+            Console.WriteLine($"b.average: {average:F2}");
+            int minMark = marks[0];
+            int maxMark = marks[0];
+            foreach(int mark in marks)
+            {
+                if (mark < minMark)
+                    minMark = mark;
+                if (mark < maxMark)
+                    maxMark = mark;
+            }
+            Console.WriteLine($"c.Minimum marks: {minMark}");
+            Console.WriteLine($"c.Maximum marks:{maxMark}");
+            Array.Sort(marks);
+            Console.WriteLine("e. Display marks in ascending order:");
+            foreach (int mark in marks)
+            {
+                Console.WriteLine(mark + " ");
+
+            }
+            Console.WriteLine();
+            Array.Reverse(marks);
+            Console.WriteLine("f. Display marks in descending order:");
+            foreach (int mark in marks)
+            {
+                Console.WriteLine(mark + " ");
+
+            }
+
+
+
+
+        }
+    }
+}
