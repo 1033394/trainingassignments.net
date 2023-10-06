c57198404a6769c2b2400c391d516d8a3e375c47
1033394 <144168815+1033394@users.noreply.github.com>
2023-10-06 14:55:40 +0530

    Add files via upload

---
+++ csharp assignments/assignment8/shapespattern/shapespattern/shapes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shapesfactory
+{
+    public interface Ishapes
+    {
+        string Getshape();
+        double GetArea();
+        double Getcircumference();
+      
+    }
+}
+
---
+++ csharp assignments/assignment8/shapespattern/shapespattern/triangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shapesfactory
+{
+    public class Triangle : Ishapes
+    {
+        public double side1 { get; }
+        public double side2 { get; }
+        public double side3 { get; }
+        public Triangle(double side1, double side2, double side3)
+        {
+            side1 = side1;
+            side2 = side2;
+            side3 = side3;
+        }
+
+        public Triangle()
+        {
+        }
+
+        public string Getshape()
+        {
+            return "triangle";
+        }
+
+        public double GetArea()
+        {
+            double s = (side1 + side2 + side3) / 2;
+            return Math.Sqrt(s * (s - side1) * (s - side2) * (s - side3));
+        }
+
+        public double Getcircumference()
+        {
+            return side1 + side2 + side3;
+        }
+    }
+}
