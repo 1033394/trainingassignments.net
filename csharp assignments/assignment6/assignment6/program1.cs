9f1c01f4fb3162cdd1ad56ada801b61ff792f904
1033394 <144168815+1033394@users.noreply.github.com>
2023-10-06 14:52:48 +0530

    Add files via upload

---
+++ csharp assignments/assignment6/assignment6/program1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using concession;
+
+namespace assignment6
+{
+    class program1
+    {
+        private const double totalfare = 500.0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("enter your age:");
+            int age =int.Parse( Console.ReadLine());
+            concession.concessioncalculator calculator = new concession.concessioncalculator();
+            string result = calculator.calculateconcession(age, name);
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
---
+++ csharp assignments/assignment6/concessionexample/program1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace concession
+
+{
+    public class concessioncalculator
+    {
+        private const double totalfare = 500.0;
+        public string calculateconcession(int age,string name)
+        {
+            if (age <= 5)
+            {
+                return $"{ name},age:{ age},ticket:little champs-free ticket";
+            }
+            else if (age > 60)
+            {
+                double seniorcitizenfare = 0.7 * totalfare;
+                return $"{ name},age:{ age},ticket:senior citizen {seniorcitizenfare:c}";
+            }
+            else
+            {
+                return $"{ name},age:{ age},ticket:ticket booked-{totalfare:c}";
+            }
+        }
+    }
+}
+
