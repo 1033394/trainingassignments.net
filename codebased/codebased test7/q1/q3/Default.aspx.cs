11741f76363dc8d2b30a20f7e02511f60944e526
1033394 <144168815+1033394@users.noreply.github.com>
2023-10-30 11:30:49 +0530

    Add files via upload

---
+++ codebased/codebased test7/q1/q3/Default.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace q3
+{
+    public partial class _Default : Page
+    {
+        protected void validateBtn_Click(object sender, EventArgs e)
+        {
+            string name = this.name.Text;
+            string family = this.family.Text;
+            // get other input values
+
+            bool valid = true;
+
+            if (name == family)
+            {
+                valid = false;
+            }
+
+            if (address.MaxLength < 2)
+            {
+                valid = false;
+            }
+
+            // other validations
+
+            if (valid)
+            {
+                result.Text = "Input is valid";
+            }
+            else
+            {
+                result.Text = "Input is NOT valid";
+            }
+        }
+    }
+}
